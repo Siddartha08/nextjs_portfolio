0b07529f2d0b20aee091f091d9cc5607f8dec55c
= <=>
2024-09-07 09:21:45 -0500

    Initial commit from Create Next App

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+<<<<<<< HEAD
   compiler: {
     styledComponents: true,
   }
+=======
+>>>>>>> 4552386 (Initial commit from Create Next App)
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { AuthProvider } from '../context/AuthContext';
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,4 +9,10 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
     </AuthProvider>
   );
+=======
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+>>>>>>> 4552386 (Initial commit from Create Next App)
 }
