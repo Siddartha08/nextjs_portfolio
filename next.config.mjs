680f5f38cf058757395d9e502a411565075ffa86
= <=>
2024-09-08 22:47:22 -0500

    rebase commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
   compiler: {
     styledComponents: true,
   }
 =======
 >>>>>>> 4552386 (Initial commit from Create Next App)
+=======
+  compiler: {
+    styledComponents: true,
+  }
+>>>>>>> 189be11 (initial commit of nextjs portfolio)
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { AuthProvider } from '../context/AuthContext';
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,9 +11,21 @@ export default function App({ Component, pageProps }) {
     </AuthProvider>
   );
 =======
+=======
+import { AuthProvider } from '../context/AuthContext';
+>>>>>>> 189be11 (initial commit of nextjs portfolio)
 import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
+<<<<<<< HEAD
   return <Component {...pageProps} />;
 >>>>>>> 4552386 (Initial commit from Create Next App)
+=======
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+>>>>>>> 189be11 (initial commit of nextjs portfolio)
 }
